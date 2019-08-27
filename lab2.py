7f98b61ec8692bacc964c6e59567e6562ae3f08c
SAHIL JAISWAL <35723299+9123165636@users.noreply.github.com>
2019-08-27 12:30:32 +0530

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,7 @@
+Python 3.7.3 (default, Apr  3 2019, 05:39:12) 
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> text = "sahil is good boy"
+>>> print(text.split())
+['sahil', 'is', 'good', 'boy']
+>>> 
